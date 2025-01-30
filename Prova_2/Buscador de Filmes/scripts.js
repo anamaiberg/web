079e8864a502e5b6d09374ef20c8cc4505cba94a
Ana Clara Maiberg <108688584+anamaiberg@users.noreply.github.com>
2025-01-29 23:56:07 -0300

    Add files via upload

---
+++ Prova_2/Buscador de Filmes/scripts.js
@@ -0,0 +1,49 @@
+const apikey = '7b6e242c';
+     
+function searchMovies(){
+    let inputText=document.querySelector("#key");
+    const search = inputText.value;
+
+    if(search == ''){
+        alert('Preencha o campo!');
+        return;
+    }
+
+    fetch(`https://www.omdbapi.com/?s=${search}&apikey=${apikey}`)
+        .then(response => response.json())
+        .then(json => showsList(json));
+}
+
+function showsList(json){
+    const moviesList = document.querySelector('#movies');
+    moviesList.innerHTML = '';
+
+    if(json.Response == 'False'){
+        alert('Nenhum filme encontrado!');
+        return;
+    }
+
+    json.Search.forEach(element => {
+        console.log(element);
+        let item = document.createElement('div');
+        item.classList.add('col');
+
+        item.innerHTML = `
+        <div class="col">
+          <div class="card h-100">
+            <img src="${element.Poster}" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">${element.Title}</h5>
+              <p class="card-text">${element.Type} - ${element.Year}</p>
+            </div>
+          </div>
+        </div>`
+
+        moviesList.appendChild(item);
+    }); 
+}
+
+document.querySelector("#searchButton").addEventListener("click", function(event) {
+    event.preventDefault(); 
+    searchMovies();
+});
